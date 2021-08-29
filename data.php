71d1d3184d13cae3f9fc845e7fd343eed4b5cead
jKaushalb <63168354+jKaushalb@users.noreply.github.com>
2021-08-30 01:12:13 +0530

    added directory functionality
    now script will open all the files from given path directory and add the records in particular table.

--- data.php
+++ data.php
@@ -4,66 +4,51 @@
             include "db_connection.php";
             
         ?>
-<head>
-	<style>
-		table,th,td
-		{
-			border: 3px solid black;
-			border-collapse: collapse;
-			background-color:skyblue;
-			padding: 10px;
-		}
-	</style>
-</head>
 
 <body>
 
  <?php
  
 				$conn=OpenCon();
-				
-				
-                if (file_exists('records.dat'))
-				{	
-					$f=fopen('records.dat','r');
-					
-					
-					while(!feof($f))
-					{
-
-						$d= fgets($f);
-						$s=explode('|',$d);
-						
-						gettype($s[1]);
-						if( $s[1] == 'D')
-						{
-							
-							
-							create($conn,$s[9]);
-							if(insert($conn,$s[9],$s))
-							{
-								echo "record.".implode(" ",$s)."inserted succesfully ";
-							}
-							echo" <br >";
-							
-								
-						}
-						
-							
-						
-						
-					}
-
-							
-						
-					//echo "</table>";
-				}
-				else
+				$path    = './records';
+				$files = scandir($path);
+				$files = array_diff(scandir($path), array('.', '..'));
+				print_r($files);
+				foreach( $files as $fi)
 				{
-					echo "File not found<br>";
+					if (file_exists($path."/".$fi))
+					{	
+						$f=fopen($path."/".$fi,'r');
+						
+						
+						while(!feof($f))
+						{
+
+							$d= fgets($f);
+							$s=explode('|',$d);
+							
+							gettype($s[1]);
+							if( $s[1] == 'D')
+							{
+								create($conn,$s[9]);
+								if(insert($conn,$s[9],$s))
+								{
+									echo "record.".implode(" ",$s)."inserted succesfully ";
+								}
+								echo" <br >";
+							}
+							
+						}
+
+					}
+					
+					else
+					{
+						echo "File not found<br>";
+					}
 				}
             ?>
             
-</div>
+
 </body>
 </html>
